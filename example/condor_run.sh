2d9e313a36a339aacac15214f0939f2d783abb28
Arni Magnusson <thisisarni@gmail.com>
2023-02-20 17:50:46 +1100

    Ensure that scripts can be run

--- example/condor_run.sh
+++ example/condor_run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+export PATH=.:$PATH
 tar -xzf Start.tar.gz
+dos2unix *.sh *doitall*
 chmod 755 *.sh *doitall* mfclo*
-./doitall.swo
+doitall.swo
 tar -czf End.tar.gz --exclude '*.tar.gz' *
