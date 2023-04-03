ed3b80d561efe5867907efc71553d03b450c72d9
Arni Magnusson <thisisarni@gmail.com>
2023-04-03 16:23:15 +1100

    Remove folder

--- example/condor.R
+++
@@ -1,9 +0,0 @@
-library(condor)
-session <- ssh_connect("NOUOFPCALC02")
-
-condor_submit()
-condor_q()
-condor_dir()
-
-# Wait until job has finished, then run
-condor_download()
--- example/condor_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export PATH=.:$PATH
-tar -xzf Start.tar.gz
-dos2unix *.sh *doitall*
-chmod 755 *.sh *doitall* mfclo*
-doitall.sh
-tar -czf End.tar.gz --exclude '*.tar.gz' --exclude '_condor_*'\
-    --exclude 'tmp' --exclude 'var' *
